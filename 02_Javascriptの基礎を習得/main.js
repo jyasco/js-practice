e722968f7580f1160f03aa898f305c66ff69bf51
鈴木茉莉花 <suzukimarika@suzukimarikanonotobukkukonpyuta.local>
2025-01-06 01:44:09 +0900

    section2-24

--- 02_Javascriptの基礎を習得/main.js
+++ 02_Javascriptの基礎を習得/main.js
@@ -1,7 +1,27 @@
 'use strict';
 
-let weight = 60;
-let height = 1.7;
+// let num = 10;
 
-let bmi = weight / (height * height);
-console.log(bmi);
+// // インクリメント
+// num++;
+// console.log(num);
+
+// // デクリメント
+// num--;
+// console.log(num);
+
+// // 前置演算子のインクリメント
+// ++num;
+// console.log(num);
+
+// // 前置演算子のデクリメント
+// --num;
+// console.log(num);
+
+let num1 = 10;
+let num2 = 10;
+let ans1 = ++num1;
+let ans2 = num2++;
+
+console.log(ans1);
+console.log(ans2);
