0c72fcfc7d6e718710bfaafaff15418fea222949
鈴木茉莉花 <suzukimarika@suzukimarikanonotobukkukonpyuta.local>
2025-01-06 01:07:31 +0900

    section2-21

--- 02_Javascriptの基礎を習得/main.js
+++ 02_Javascriptの基礎を習得/main.js
@@ -1,29 +1,12 @@
 'use strict';
 
-let ans = 1 + 2;
-console.log(ans);
+let lastName = 'すずき';
+let firstName = 'まりか';
 
-let i = 20;
-let j = 10;
-let ans1 = i + j;
-console.log(ans1);
+// 文字列結合
+let message1 = 'こんにちは ' + lastName + ' ' + firstName + ' さん';
+console.log(message1);
 
-let ans2 = i - j;
-console.log(ans2);
-
-let ans3 = i * j;
-console.log(ans3);
-
-let ans4 = i / j;
-console.log(ans4);
-
-let x = 3;
-let y = 2;
-let ans5 = x / y;
-console.log(ans5);
-
-let ans6 = x % y;
-console.log(ans6);
-
-let ans7 = Math.pow(x, y);
-console.log(ans7);
+// テンプレートリテラル
+let message2 = `こんにちは ${lastName} ${firstName} さん`;
+console.log(message2);
