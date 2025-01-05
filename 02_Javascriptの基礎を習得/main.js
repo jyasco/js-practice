a7eb850344efbf95f267703cb730f4232ddbb32a
鈴木茉莉花 <suzukimarika@suzukimarikanonotobukkukonpyuta.local>
2025-01-06 01:23:07 +0900

    section2-23

--- 02_Javascriptの基礎を習得/main.js
+++ 02_Javascriptの基礎を習得/main.js
@@ -1,26 +1,7 @@
 'use strict';
 
-// 普通の書き方
-let num = 20;
-num = num + 10;
-console.log(num);
+let weight = 60;
+let height = 1.7;
 
-// 複合代入演算子
-let num1 = 20;
-num1 += 10;
-console.log(num1);
-
-// 引き算
-let num2 = 20;
-num2 -= 10;
-console.log(num2);
-
-// 掛け算
-let num3 = 20;
-num3 *= 10;
-console.log(num3);
-
-// 割り算
-let num4 = 20;
-num4 /= 10;
-console.log(num4);
+let bmi = weight / (height * height);
+console.log(bmi);
