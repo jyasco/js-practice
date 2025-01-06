9d83b4f7fa0ab307aec54d646e6c61d94119ca0d
鈴木茉莉花 <suzukimarika@suzukimarikanonotobukkukonpyuta.local>
2025-01-06 18:56:22 +0900

    section2-33

--- 02_Javascriptの基礎を習得/main.js
+++ 02_Javascriptの基礎を習得/main.js
@@ -1,14 +1,46 @@
 'use strict';
-// 例題）2の倍数、かつ、3の倍数のときに、
-// メッセージを表示する
+// 例題）入賞メッセージの出力。
+// 順位numによって、出力するメッセージを変えるプログラム
+// 1 → 金賞
+// 2 → 銀賞
+// 3 → 銅賞
+// 4 → 4位
+// 5 → 5位
+// それ以外の値 → 選外
 
-let num = 12;
-if(num % 2 === 0) {
-  if (num % 3 === 0){
-    console.log(`${num}は２の倍数かつ３の倍数です`);
-  }
+let num = 6;
+switch(num) {
+  case 1:
+    console.log('金賞');
+    break;
+  case 2:
+    console.log('銀賞');
+    break;
+  case 3:
+    console.log('銅賞');
+    break;
+  case 4:
+    console.log('4位');
+    break;
+  case 5:
+    console.log('5位');
+    break;
+  default:
+    console.log('選外');
+    // break;
 }
 
-if(num % 2 === 0 && num % 3 === 0){
-  console.log(`${num}は２の倍数かつ３の倍数です`);
+
+if(num === 1) {
+  console.log('金賞');
+} else if(num === 2) {
+  console.log('銀賞');
+} else if(num === 3 ) {
+  console.log('銅賞');
+} else if(num === 4) {
+  console.log('4位');
+} else if(num === 5) {
+  console.log('5位');
+} else {
+  console.log('選外');
 }
