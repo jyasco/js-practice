c90f190f3ddd884643d2af535fe65e7968937af5
鈴木茉莉花 <suzukimarika@suzukimarikanonotobukkukonpyuta.local>
2025-01-06 18:08:22 +0900

    section2-30

--- 02_Javascriptの基礎を習得/main.js
+++ 02_Javascriptの基礎を習得/main.js
@@ -1,10 +1,16 @@
 'use strict';
 
-let b = false;
-// if(b){
-//   console.log('はい');
-// } else {
-//   console.log('いいえ');
-// }
+let num = 100;
+let str = '100';
 
-console.log(!b);
+if(num == str){
+  console.log('同じ');
+} else {
+  console.log('同じではない');
+}
+
+if(num === str){
+  console.log('同じ');
+} else {
+  console.log('同じではない');
+}
