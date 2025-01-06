b066dc70100e7d3c70ee5a755b9d643f8c53a09f
鈴木茉莉花 <suzukimarika@suzukimarikanonotobukkukonpyuta.local>
2025-01-06 19:47:52 +0900

    section3-40

--- 02_Javascriptの基礎を習得/main.js
+++ 02_Javascriptの基礎を習得/main.js
@@ -1,9 +1,7 @@
 'use strict';
-// 例題
-// 年齢が20歳以上の場合はビールと出力
-// 年齢が20歳未満の場合はコーラと出力
+// 例題：四角形の面積を求める関数
+function rectangle(height, width){
+  return height * width;
+}
 
-let age = 20;
-let beverage = (age >= 20) ? 'ビール' : 'コーラ';
-
-console.log(beverage);
+console.log(rectangle(3,5));
