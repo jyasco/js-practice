d5189a1a6a4c7b8e9f2fba9bbbac8e906bd72e9b
鈴木茉莉花 <suzukimarika@suzukimarikanonotobukkukonpyuta.local>
2025-01-06 23:52:29 +0900

    section3-50

--- 02_Javascriptの基礎を習得/main.js
+++ 02_Javascriptの基礎を習得/main.js
@@ -1,46 +1,48 @@
 'use strict';
-// 宣言、初期化
-let colors = ['Red','Green','Blue'];
 
-console.log(colors);
-
-// 末尾に追加
-colors.push('Black');
-console.log(colors);
-
-// 先頭に追加
-colors.unshift('white');
-console.log(colors);
-
-// 更新
-colors[1] = 'Aka';
-console.log(colors);
+// let user = new Object;
+// user.name = 'Taro Yamada';
+// user.gender = 'man'
+// user.birthYear = 1990;
+// console.log(user);
 
 
-// 指定した位置に1つ以上の値を挿入
-let insertArr = ['a', 'b', 'c'];
-insertArr.splice(1, 0, 'D');
-console.log(insertArr);
+// オブジェクト初期化子
+let user = { name: 'Taro Yamada', gender: 'man', birthYear: 1999 };
+console.log(user);
 
-// 複数挿入
-insertArr.splice(2, 0, 'X', 'Y', 'Z');
-console.log(insertArr);
+// ドット記法
+console.log(user.name);
+console.log(user.gender);
+console.log(user.birthYear);
 
-// 指定した位置から削除
-insertArr.splice(2, 2)
-console.log(insertArr);
+// ブラケット記法
+console.log(user['name']);
+console.log(user['gender']);
+console.log(user['birthYear']);
+
+// 個数
+console.log(Object.keys(user).length);
+
+// プロパティ(key)を取得
+let keys = Object.keys(user);
+console.log(keys);
+
+// keyの追加
+user.prefecture = 'Kanagawa';
+console.log(user);
+
+// 値の書き換え
+user.prefecture = 'Chiba';
+console.log(user);
 
 // 結合
-let arr1 =['a', 'b', 'c'];
-let arr2 =['d', 'e', 'f'];
-let mergedArr = arr1.concat(arr2);
-console.log(mergedArr);
+let obj1 = { a: 1,b: 2,c: 3 };
+let obj2 = { d: 4,e: 5,f: 6 };
+let mergedObj = Object.assign(obj1, obj2);
+console.log(mergedObj);
 
-// 先頭を削除
-let arr3 =['a', 'b', 'c', 'd'];
-arr3.shift();
-console.log(arr3);
 
-// 末尾を削除
-arr3.pop();
-console.log(arr3);
+// 空のオブジェクト
+let person = {};
+console.log(person);
