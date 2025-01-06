bbbc337e95da73c7647e77dc0cb1c252d5b25fab
鈴木茉莉花 <suzukimarika@suzukimarikanonotobukkukonpyuta.local>
2025-01-06 18:15:34 +0900

    section2-30:演習

--- 02_Javascriptの基礎を習得/main.js
+++ 02_Javascriptの基礎を習得/main.js
@@ -1,16 +1,23 @@
 'use strict';
+// 演習問題：
+// Aさんの 体重は60kg, 身長は1.7mです。
+// ①復習：AさんのBMIを求めて出力
+// BMI = 体重kg / (身長m * 身長m)
+// ②BMIの数値により「痩せ」「普通」「肥満」を出力
+// 25以上が「肥満」
+// 18.5以上25未満が「普通」
+// 18.5未満が「痩せ」
 
-let num = 100;
-let str = '100';
+let mass = 60;
+let height = 1.7;
+let bmi = mass / (height * height);
 
-if(num == str){
-  console.log('同じ');
+console.log(bmi);
+
+if (bmi >= 25){
+  console.log('肥満');
+} else if(bmi >= 18.5){
+  console.log('普通');
 } else {
-  console.log('同じではない');
-}
-
-if(num === str){
-  console.log('同じ');
-} else {
-  console.log('同じではない');
+  console.log('痩せ');
 }
