7b24c0258066c83507ccbac12dcb561d4da4d3b3
鈴木茉莉花 <suzukimarika@suzukimarikanonotobukkukonpyuta.local>
2025-01-06 21:03:20 +0900

    section3-45

--- 02_Javascriptの基礎を習得/main.js
+++ 02_Javascriptの基礎を習得/main.js
@@ -1,13 +1,16 @@
 'use strict';
-// 例題：四角形の面積を求める関数
-// const getRectangle = function(height, width){
-//   return height * width;
-// }
+// 演習問題「みかんの合計金額を計算するプログラム」
+// Aさんが1個100円のみかんを、20個買いました。 
+// みかんの単価と個数を引数に渡すと、
+// 合計金額を計算して返す関数を作成。
+// 作成した関数を呼び出して、
+// Aさんの支払合計金額はいくらになるか計算して、
+// コンソールに出力する。
+// ※消費税は考慮しないものとする。 
+// 回答例では、function 命令で定義する方法で、実装。
 
-// console.log(getRectangle(3, 5));
-
-const getRectangle = (height, width) => {
-  return height * width;
+function getTotalPrice(price, n){
+  return price * n;
 }
 
-console.log(getRectangle(3, 5));
+console.log(getTotalPrice(100, 20));
