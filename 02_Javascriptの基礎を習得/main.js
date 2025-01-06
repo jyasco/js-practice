6083456e27d965fbef3cf79aa5cf71df92882a52
鈴木茉莉花 <suzukimarika@suzukimarikanonotobukkukonpyuta.local>
2025-01-07 02:38:19 +0900

    section3-54

--- 02_Javascriptの基礎を習得/main.js
+++ 02_Javascriptの基礎を習得/main.js
@@ -1,32 +1,9 @@
 'use strict';
 
-// 例題1) while文を使って
+// 例題1) do while文を使って
 // コンソールに1~3の数字を表示
-
 let i = 1;
-while(i <= 3){
+do {
   console.log(i);
   i++;
-}
-
-
-// 例題2) whileループの途中でwhile文を抜ける
-// (jが5になったらwhile文を抜ける)
-
-// let j = 1;
-// while ( j <= 10 ){
-//   console.log(j);
-//   if(j === 5){
-//     break;
-//   }
-//   j++;
-// }
-
-let j = 1;
-while ( j <= 10 ){
-  console.log(j);
-  j++;
-  if(j === 5){
-    break;
-  }
-}
+} while (i <= 3);
