d1f102f385fa6010a8864d37d1d46ccecda5092f
鈴木茉莉花 <suzukimarika@suzukimarikanonotobukkukonpyuta.local>
2025-01-07 01:55:38 +0900

    section3-52

--- 02_Javascriptの基礎を習得/main.js
+++ 02_Javascriptの基礎を習得/main.js
@@ -1,23 +1,34 @@
 'use strict';
 
-// 例題1) 繰り返し処理forを使って
-// コンソールに1から3まで順に表示
-
-for( let i = 1; i <= 3; i++ ){
-  console.log(i);
+for( let i = 0; i < 3; i++ ){
+  for( let j = 0; j < 3; j++ ){
+    console.log(`i: ${i}, j: ${j}`);
+  }
 }
 
+// 例題) 2次元配列(配列の中に配列を入れる構造)に格納されている
+// テストの点数を全て取り出して表示する
 
-// 例題2) 配列['a', 'b', 'c']の値を
-// for文を利用して
-// 1つずつコンソールに出力
+const scores = [
+  [100, 99, 98],
+  [90, 89, 88],
+  [80, 79, 78]
+];
 
-let arr = ['a', 'b', 'c'];
+// for文使わない場合
+// console.log(scores[0][0]);
+// console.log(scores[0][1]);
+// console.log(scores[0][2]);
+// console.log(scores[1][0]);
+// console.log(scores[1][1]);
+// console.log(scores[1][2]);
+// console.log(scores[2][0]);
+// console.log(scores[2][1]);
+// console.log(scores[2][2]);
 
-for ( let i = 0; i < arr.length; i++ ){
-  console.log(arr[i]);
-}
-
-for ( let i = 0; i <= arr.length-1; i++ ){
-  console.log(arr[i]);
+// for文使う場合
+for( let i = 0; i < scores.length; i++ ){
+  for( let j = 0; j < scores[i].length; j++ ){
+    console.log(scores[i][j]);
+  }
 }
