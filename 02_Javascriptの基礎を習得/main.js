85e0a82ee80763e1786178272c8d7f3c9a97702b
鈴木茉莉花 <suzukimarika@suzukimarikanonotobukkukonpyuta.local>
2025-01-07 02:27:00 +0900

    section3-53

--- 02_Javascriptの基礎を習得/main.js
+++ 02_Javascriptの基礎を習得/main.js
@@ -1,34 +1,32 @@
 'use strict';
 
-for( let i = 0; i < 3; i++ ){
-  for( let j = 0; j < 3; j++ ){
-    console.log(`i: ${i}, j: ${j}`);
-  }
+// 例題1) while文を使って
+// コンソールに1~3の数字を表示
+
+let i = 1;
+while(i <= 3){
+  console.log(i);
+  i++;
 }
 
-// 例題) 2次元配列(配列の中に配列を入れる構造)に格納されている
-// テストの点数を全て取り出して表示する
 
-const scores = [
-  [100, 99, 98],
-  [90, 89, 88],
-  [80, 79, 78]
-];
+// 例題2) whileループの途中でwhile文を抜ける
+// (jが5になったらwhile文を抜ける)
 
-// for文使わない場合
-// console.log(scores[0][0]);
-// console.log(scores[0][1]);
-// console.log(scores[0][2]);
-// console.log(scores[1][0]);
-// console.log(scores[1][1]);
-// console.log(scores[1][2]);
-// console.log(scores[2][0]);
-// console.log(scores[2][1]);
-// console.log(scores[2][2]);
+// let j = 1;
+// while ( j <= 10 ){
+//   console.log(j);
+//   if(j === 5){
+//     break;
+//   }
+//   j++;
+// }
 
-// for文使う場合
-for( let i = 0; i < scores.length; i++ ){
-  for( let j = 0; j < scores[i].length; j++ ){
-    console.log(scores[i][j]);
+let j = 1;
+while ( j <= 10 ){
+  console.log(j);
+  j++;
+  if(j === 5){
+    break;
   }
 }
