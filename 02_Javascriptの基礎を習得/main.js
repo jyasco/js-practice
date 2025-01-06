689ad42fa33617681efed353a4b0c4c1c7b2b69a
鈴木茉莉花 <suzukimarika@suzukimarikanonotobukkukonpyuta.local>
2025-01-06 17:50:54 +0900

    section2-27

--- 02_Javascriptの基礎を習得/main.js
+++ 02_Javascriptの基礎を習得/main.js
@@ -1,22 +1,8 @@
 'use strict';
 
-// 例題
-// テストの点数xによって
-// 通知表の成績をコンソールに
-// 出力するプログラムを書く
+let birthYear = '2000';
+let age = 18;
 
-// 90点以上 A
-// 80点以上 B
-// 60点以上 C
-// 60点未満 D
+console.log(Number(birthYear)+ age);
 
-let x = 90;
-if (x >= 90){
-  console.log('A');
-} else if (x >= 80){
-  console.log('B');
-} else if (x >= 60){
-  console.log('C');
-} else {
-  console.log('D');
-}
+console.log(Number('Suzuki'));
