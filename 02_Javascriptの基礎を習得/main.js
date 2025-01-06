b68f38b792cf0684b1da9620db1c8b53a4dd2e46
鈴木茉莉花 <suzukimarika@suzukimarikanonotobukkukonpyuta.local>
2025-01-06 17:24:34 +0900

    section2-25

--- 02_Javascriptの基礎を習得/main.js
+++ 02_Javascriptの基礎を習得/main.js
@@ -1,27 +1,13 @@
 'use strict';
 
-// let num = 10;
+let temperature = 29;
+if (temperature > 28 ){
+  console.log('冷房稼働！');
+}
 
-// // インクリメント
-// num++;
-// console.log(num);
-
-// // デクリメント
-// num--;
-// console.log(num);
-
-// // 前置演算子のインクリメント
-// ++num;
-// console.log(num);
-
-// // 前置演算子のデクリメント
-// --num;
-// console.log(num);
-
-let num1 = 10;
-let num2 = 10;
-let ans1 = ++num1;
-let ans2 = num2++;
-
-console.log(ans1);
-console.log(ans2);
+let g = 101;
+if (g >= 100){
+  console.log('100以上');
+} else {
+  console.log('100以上では無い');
+} 
