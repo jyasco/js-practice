fd13c0e64c07a9815b3d6c319c950f51a4bd367c
鈴木茉莉花 <suzukimarika@suzukimarikanonotobukkukonpyuta.local>
2025-01-07 19:17:30 +0900

    section4-61

--- 02_Javascriptの基礎を習得/main.js
+++ 02_Javascriptの基礎を習得/main.js
@@ -1,11 +1,9 @@
 'use strict';
 
-let btn = document.getElementById('triggerButton');
+let btn = document.getElementById('triggerButton'); //置換したいノードを取得
 btn.addEventListener('click', function(e){
-  // alert('clicked!');
   // IDによるノードの取得
   let headerTitle = document.getElementById('headerTitle');
-  console.log(headerTitle);
-  // コンテンツの取得（テキストの取得）
-  console.log(headerTitle.textContent);
+  // テキストの置換
+  headerTitle.textContent = 'タイトルが変更されました'; //取得したノードのtextContentプロパティを使用している
 }, false);
