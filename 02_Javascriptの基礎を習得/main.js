616a075df1db19ef54c4a9e47433a54161dfc191
鈴木茉莉花 <suzukimarika@suzukimarikanonotobukkukonpyuta.local>
2025-01-07 19:47:36 +0900

    section4-62

--- 02_Javascriptの基礎を習得/main.js
+++ 02_Javascriptの基礎を習得/main.js
@@ -1,9 +1,16 @@
 'use strict';
 
-let btn = document.getElementById('triggerButton'); //置換したいノードを取得
+let btn = document.getElementById('trigggerButton');
 btn.addEventListener('click', function(e){
   // IDによるノードの取得
-  let headerTitle = document.getElementById('headerTitle');
-  // テキストの置換
-  headerTitle.textContent = 'タイトルが変更されました'; //取得したノードのtextContentプロパティを使用している
+  let inputColorName = document.getElementById('inputColorName');
+  // 取得したinput要素の入力値を取得
+  let colorNameText = inputColorName.value;
+  // 取得したinput要素のname属性を取得
+  let inputName = inputColorName.getAttribute('name');
+  // idがshowTextのp要素を取得
+  let showText = document.getElementById('showText');
+  // 出力
+  showText.textContent = `name属性:${inputName}, 入力されたテキスト:${colorNameText}`;
+
 }, false);
