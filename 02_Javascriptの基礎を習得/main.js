922aa8439e6aecb944de10c86a50d7862d13cb98
鈴木茉莉花 <suzukimarika@suzukimarikanonotobukkukonpyuta.local>
2025-01-07 23:57:01 +0900

    section4-68

--- 02_Javascriptの基礎を習得/main.js
+++ 02_Javascriptの基礎を習得/main.js
@@ -1,14 +1,33 @@
 'use strict';
 
-let btn = document.getElementById('triggerButton');
-btn.addEventListener('click', function(e){
-  // let element = document.querySelector('.foo');
-  // console.log(element);
-  // console.log(element.textContent);
-
-  let elements = document.querySelectorAll('.foo');
-  console.log(elements);
-  for( let i = 0; i < elements.length; i++ ){
-    console.log(elements[i].textContent);
+// 追加処理
+let addButton = document.getElementById('addButton');
+addButton.addEventListener('click', function(e){
+  // 入力欄のinput要素を取得
+  let inputLanguage = document.getElementById('inputLanguage');
+  // input要素から文字列を取得
+  let language = inputLanguage.value;
+  if( language === '' ){
+    alert('未入力です');
+    return;
   }
+  // li要素を作成
+  let listItem = document.createElement('li');
+  // li要素のコンテンツに入力された文字列を設定
+  listItem.textContent = language;
+  // ul要素を取得
+  let languageList = document.getElementById('languageList');
+  // リストの末尾に、生成したli要素を追加
+  languageList.appendChild(listItem);
+  // 入力欄をクリア
+  inputLanguage.value = '';
 }, false);
+
+// 削除処理
+let removeButton = document.getElementById('removeButton');
+removeButton.addEventListener('click', function(e){
+  // ul要素を取得
+  let languageList = document.getElementById('languageList');
+  // リストの末尾から要素を削除
+  languageList.removeChild(languageList.lastElementChild);
+}, false); 
