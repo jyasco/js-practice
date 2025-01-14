7f50fd8a521fbff5e76bd74b538539088c2b236c
鈴木茉莉花 <suzukimarika@suzukimarikanonotobukkukonpyuta.local>
2025-01-15 08:06:06 +0900

    section5-76

--- 02_Javascriptの基礎を習得/main.js
+++ 02_Javascriptの基礎を習得/main.js
@@ -1,17 +1,16 @@
 'use strict';
-// イベントハンドラ
-function handleChange(){
-  // 「全てにチェックボックス」の要素を取得
-  const checkAll = document.getElementById('checkAll');
-  // name属性がlanguageのチェックボックス要素を取得
-  const checkBoxes = document.getElementsByName('language');
-  // name属性がjanguageのチェックボックス要素をループ処理
-  for( let i = 0; i < checkBoxes.length; i++ ){
-    // 各チェックボックスのチェック状態を、「全てチェック」の状態に合わせる
-    checkBoxes[i].checked = checkAll.checked;
-  }
-}
+// 例題
+// 「Now Loading...」表示 + 画像表示をする
+// DOMContentLoadedイベントが発生したら、alertダイアログを表示する。
+// loadイベントが発生したら、「Now Loading...」を非表示にする
 
-// checkAllボックスのchangeイベントのイベントハンドラを登録
-const ca = document.getElementById('checkAll');
-ca.addEventListener('click', handleChange, false);
+// DOMContentLoaded
+document.addEventListener('DOMContentLoaded', function(){
+  alert('DOMContentLoaded');
+}, false);
+
+// loadイベントが呼ばれたらNowLoadingを非表示
+window.onload = function(){
+  const nowLoading = document.getElementById('nowLoading');
+  nowLoading.style.display = 'none'
+}
