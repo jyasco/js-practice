9305015eaf21686713317a5e63a4987a5d5941bf
鈴木茉莉花 <suzukimarika@suzukimarikanonotobukkukonpyuta.local>
2025-01-14 23:29:56 +0900

    section5-74

--- 02_Javascriptの基礎を習得/main.js
+++ 02_Javascriptの基礎を習得/main.js
@@ -1,21 +1,13 @@
 'use strict';
 const sayHelloButton = document.getElementById('sayHelloButton');
-// sayHelloButton.addEventListener('click', function(e){
-//   console.log('Hello!');
-//   console.log(e);
-// }, false);
-
-// sayHelloButton.addEventListener('click', function(){
-//   console.log('Hello!');
-// }, false);
-
-// sayHelloButton.addEventListener('click', (e) => {
-//   console.log('Hello!');
-//   console.log(e);
-// }, false);
-
 function sayHelloEventHandler(e) {
   console.log('Hello');
-  console.log(e);
 }
 sayHelloButton.addEventListener('click', sayHelloEventHandler, false);
+
+const removeEventHandlerButton = document.getElementById('removeEventHandlerButton');
+function removeEventHandler(e){
+  console.log('remove');
+  sayHelloButton.removeEventListener('click', sayHelloEventHandler, false);
+}
+removeEventHandlerButton.addEventListener('click', removeEventHandler, false);
