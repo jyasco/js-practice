13b9bba11fe03d4a0a6c7f47a0c0b2e0f97af541
鈴木茉莉花 <suzukimarika@suzukimarikanonotobukkukonpyuta.local>
2025-01-15 00:42:08 +0900

    section5-76

--- 02_Javascriptの基礎を習得/main.js
+++ 02_Javascriptの基礎を習得/main.js
@@ -1,13 +1,17 @@
 'use strict';
-const sayHelloButton = document.getElementById('sayHelloButton');
-function sayHelloEventHandler(e) {
-  console.log('Hello');
+// イベントハンドラ
+function handleChange(){
+  // 「全てにチェックボックス」の要素を取得
+  const checkAll = document.getElementById('checkAll');
+  // name属性がlanguageのチェックボックス要素を取得
+  const checkBoxes = document.getElementsByName('language');
+  // name属性がjanguageのチェックボックス要素をループ処理
+  for( let i = 0; i < checkBoxes.length; i++ ){
+    // 各チェックボックスのチェック状態を、「全てチェック」の状態に合わせる
+    checkBoxes[i].checked = checkAll.checked;
+  }
 }
-sayHelloButton.addEventListener('click', sayHelloEventHandler, false);
 
-const removeEventHandlerButton = document.getElementById('removeEventHandlerButton');
-function removeEventHandler(e){
-  console.log('remove');
-  sayHelloButton.removeEventListener('click', sayHelloEventHandler, false);
-}
-removeEventHandlerButton.addEventListener('click', removeEventHandler, false);
+// checkAllボックスのchangeイベントのイベントハンドラを登録
+const ca = document.getElementById('checkAll');
+ca.addEventListener('click', handleChange, false);
