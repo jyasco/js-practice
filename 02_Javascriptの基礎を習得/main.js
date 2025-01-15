2383589d305c9ee46d95e2f78b798d85f69f2fe3
鈴木茉莉花 <suzukimarika@suzukimarikanonotobukkukonpyuta.local>
2025-01-15 09:04:20 +0900

    section6-82

--- 02_Javascriptの基礎を習得/main.js
+++ 02_Javascriptの基礎を習得/main.js
@@ -3,6 +3,7 @@
 function keyUp(e) {
   // 入力された値を取得
   let str = inputText.value;
+  str = str.replace(/\r?\n/g, ''); // 第一引数を第二引数に置換する
   // 残りの文字数
   let num = 10 - str.length;
   console.log(num);
@@ -10,6 +11,14 @@ function keyUp(e) {
   const characterCount = document.getElementById('characterCount');
   // 残りの文字数をセットして表示
   characterCount.textContent = num;
+  // 残り文字数表示部分のp要素を取得
+  const characterCountWrap = document.getElementById('characterCountWrap');
+  // 文字色の設定
+  if(num >= 0){
+    characterCountWrap.style.color = 'black';
+  } else {
+    characterCountWrap.style.color = 'red';
+  }
 }
 
 const inputText = document.getElementById('inputText');
