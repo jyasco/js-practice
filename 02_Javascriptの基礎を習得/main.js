3d213b438f59f5296c861404562abdfbb8d846c5
鈴木茉莉花 <suzukimarika@suzukimarikanonotobukkukonpyuta.local>
2025-01-22 10:07:02 +0900

    section7-87

--- 02_Javascriptの基礎を習得/main.js
+++ 02_Javascriptの基礎を習得/main.js
@@ -1,5 +1,4 @@
 'use strict';
-'use strict';
 const images = [
   {src: 'https://learnwithnakamura.s3.ap-northeast-1.amazonaws.com/javascript/C0zDWAPFT9A.jpg', description: '猫の画像1'},
   {src: 'https://learnwithnakamura.s3.ap-northeast-1.amazonaws.com/javascript/fEK4jvgnApg.jpg', description: '猫の画像2'},
@@ -7,3 +6,17 @@ const images = [
   {src: 'https://learnwithnakamura.s3.ap-northeast-1.amazonaws.com/javascript/e-S23SJzFqs.jpg', description: '猫の画像4'},
   {src: 'https://learnwithnakamura.s3.ap-northeast-1.amazonaws.com/javascript/Hnwm8ktAd6E.jpg', description: '猫の画像5'}
 ];
+
+// メイン画像の初期値を設定
+let image = document.createElement('img'); //image要素を生成
+image.setAttribute('src', images[0].src); //image要素のsrc属性を設定
+image.setAttribute('alt', images[0].description); //image要素のalt属性を設定
+
+// 説明の初期値を設定
+let description = document.createElement('p');
+description.textContent = image.alt;
+
+// 作成した要素を表示する
+let mainImage = document.getElementById('main_image');
+mainImage.insertBefore(image, null);
+mainImage.insertBefore(description, null);
